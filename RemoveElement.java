6ef7f008b7d0433378a985f8c18d7cd0305f2347
Ritik Billore <101549076+Ritikbillore18@users.noreply.github.com>
2024-01-07 23:41:11 +0530

    Remove Element

---
+++ RemoveElement.java
@@ -0,0 +1,12 @@
+// Remove Element
+class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int x : nums){
+            if (x != val) {
+                nums[k++] = x;
+            }
+        }
+        return k;
+    }
+}
